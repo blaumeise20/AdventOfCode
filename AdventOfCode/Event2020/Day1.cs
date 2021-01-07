5f75eb9631bfac410a5c4accc1c474982c8dad67
blaumeise20 <62756994+blaumeise20@users.noreply.github.com>
2021-01-07 17:23:59 +0100

    Add code for day 1 - 2020

---
+++ AdventOfCode/Event2020/Day1.cs
@@ -0,0 +1,55 @@
+﻿namespace AdventOfCode.Event2020
+{
+    public class Day1 : Day<int>
+    {
+        protected override bool Part2Done => true;
+
+        protected override int MapLines(string line) => int.Parse(line);
+
+        protected override void Part1(int[] numbers)
+        {
+            var done = false;
+            for (int i = 0; i < numbers.Length - 1; i++)
+            {
+                for (int j = i + 1; j < numbers.Length; j++)
+                {
+                    var x = numbers[i];
+                    var y = numbers[j];
+                    if (x + y == 2020)
+                    {
+                        Print("Numbers found", $"{x} + {y} = 2020");
+                        Print("Resulting number", x * y, true);
+                        done = true;
+                        break;
+                    }
+                }
+                if (done) break;
+            }
+        }
+        protected override void Part2(int[] numbers)
+        {
+            var done = false;
+            for (int i = 0; i < numbers.Length - 1; i++)
+            {
+                for (int j = i + 1; j < numbers.Length; j++)
+                {
+                    for (int k = i + 1; k < numbers.Length; k++)
+                    {
+                        var x = numbers[i];
+                        var y = numbers[j];
+                        var z = numbers[k];
+                        if (x + y + z == 2020)
+                        {
+                            Print("Numbers found", $"{x} + {y} + {z} = 2020");
+                            Print("Resulting number", x * y * z, true);
+                            done = true;
+                            break;
+                        }
+                    }
+                    if (done) break;
+                }
+                if (done) break;
+            }
+        }
+    }
+}

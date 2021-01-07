6d8650acb08e17bb63c2d68edbf5d2a30e51b2ea
blaumeise20 <62756994+blaumeise20@users.noreply.github.com>
2021-01-07 17:25:01 +0100

    Add code for day 6 - 2020

---
+++ AdventOfCode/Event2020/Day6.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq;
+
+namespace AdventOfCode.Event2020
+{
+    public class Day6 : Day
+    {
+        protected override bool Part2Done => true;
+
+        protected override void Part1(string[] lines)
+        {
+            Print("Resulting count", string.Join("\n", lines).Split("\n\n").Select(l => l.Replace("\n", "").Distinct().Count()).Sum(), true);
+        }
+        protected override void Part2(string[] lines)
+        {
+            Print("Resulting count", string.Join("\n", lines).Split("\n\n").Select(l => {
+                var dist = l.Replace("\n", "").Distinct();
+                var newlines = l.Where(c => c == '\n').Count();
+                return dist.Where(d => l.Where(c => c == d).Count() == newlines + 1).Count();
+            }).Sum(), true);
+        }
+    }
+}

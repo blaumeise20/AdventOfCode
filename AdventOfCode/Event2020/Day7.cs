f8a89067c9e10d80efee91718d79d8d3edd1c6fb
blaumeise20 <62756994+blaumeise20@users.noreply.github.com>
2021-01-07 17:25:08 +0100

    Add code for day 7 - 2020

---
+++ AdventOfCode/Event2020/Day7.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace AdventOfCode.Event2020
+{
+    public class Day7 : Day
+    {
+        protected override bool Part2Done => true;
+
+        protected override string MapLines(string line) => line[0..^1];
+
+        protected override void Part1(string[] lines)
+        {
+            bool Check(string line) => !line.EndsWith("no other bags") && (line.Split(" bags contain ")[1].Contains("shiny gold") || line.Split(" bags contain ")[1].Split(", ").Any(b => Check(lines.FirstOrDefault(l => l.StartsWith(Regex.Match(b, "^\\d+ ([a-z ]+) bags?$").Groups[1].Value)))));
+            Print("Resulting count", lines.Where(Check).Count(), true);
+        }
+        protected override void Part2(string[] lines)
+        {
+            int CountInBag(string bag) => 1 + lines.FirstOrDefault(l => l.StartsWith(bag)).Split(" bags contain ")[1].Split(", ").Sum(b =>
+            {
+                var match = Regex.Match(b, "^(\\d+) ([a-z ]+) bags?$").Groups;
+                return b == "no other bags" ? 0 : int.Parse(match[1].Value) * CountInBag(match[2].Value);
+            });
+            Print("Resulting count", CountInBag("shiny gold") - 1, true);
+        }
+    }
+}

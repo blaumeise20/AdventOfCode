1932dbfe2b8fa661e48259bc97116f2f60d0dc54
blaumeise20 <62756994+blaumeise20@users.noreply.github.com>
2021-01-08 07:59:00 +0100

    Add code for day 1 - 2018

---
+++ AdventOfCode/Event2018/Day1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode.Event2018
+{
+    public class Day1 : Day<int>
+    {
+        protected override bool Part2Done => true;
+
+        protected override int MapLines(string line) => int.Parse(line);
+
+        protected override void Part1(int[] lines) => Print("Resulting number", lines.Sum(), true);
+        protected override void Part2(int[] lines)
+        {
+            var seenCurrencies = new HashSet<int>() { 0 };
+            var i = 0;
+            while (true)
+            {
+                var result = seenCurrencies.Last() + lines[i];
+                i = (i + 1) % lines.Length;
+                if (!seenCurrencies.Add(result)) { Print("Resulting number", result, true); break; }
+            }
+        }
+    }
+}

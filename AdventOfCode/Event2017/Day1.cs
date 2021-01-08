0f60a4e3a8a812f876fabad48a60c57b85568c69
blaumeise20 <62756994+blaumeise20@users.noreply.github.com>
2021-01-08 08:08:07 +0100

    Add code for day 1 - 2017

---
+++ AdventOfCode/Event2017/Day1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace AdventOfCode.Event2017
+{
+    public class Day1 : Day
+    {
+        protected override bool Part2Done => true;
+
+        protected override void Part1(string[] lines) => Print("Resulting count", Solve((i, length) => (i + 1) % length, lines[0]), true);
+        protected override void Part2(string[] lines) => Print("Resulting count", Solve((i, length) => (i + length / 2) % length, lines[0]), true);
+
+        private int Solve(Func<int, int, int> lambda, string line)
+        {
+            var length = line.Length;
+            return line.Where((c, i) => c == line[lambda(i, length)]).Select(c => int.Parse(c.ToString())).Sum();
+        }
+    }
+}
